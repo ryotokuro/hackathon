3cb029c83d08f69b5eb1bfa5322825b625ac5363
Taiyue Tan <30404851+ryotokuro@users.noreply.github.com>
2018-08-05 14:41:29 +1000

    created application, added description
    added team name, idea goal

---
+++ app.cs
@@ -0,0 +1,9 @@
+// using c-sharp on Unity
+// VR Program
+// Team: VRality
+
+// Idea: implement virtual reality environment + interactive objects in the surrounding
+createEnvironment();
+
+// Object class
+interact();
