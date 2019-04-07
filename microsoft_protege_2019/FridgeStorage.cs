f47135469027cc884fbc6367cf74800771dd59ab
ryotokuro <ryotokuro@gmail.com>
2019-04-08 09:01:31 +1000

    added heroes, innovation dojo and microsoft protege

---
+++ microsoft_protege_2019/FridgeStorage.cs
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace Microsoft.BotBuilderSamples
+{
+    public class FridgeStorage
+    {
+        public string Food { get; set; }
+        public string Type { get; set; }
+    }
+}
---
+++ microsoft_protege_2019/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild CoreBot.sln -p:DeployOnBuild=true -p:PublishProfile=protege-Web-Deploy.pubxml -p:Password=7QMqccDpvisJgvu7fHNxNBjiFDk67w6krQgrRczvpghZPZPxWPzhGwJGnPxb
+
